2ab40a5189d93c8cd2cb86f6e38cd09ee9019ad1
kietvong <67884054+kietvong@users.noreply.github.com>
2020-07-08 00:07:22 -0400

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,13 +1,15 @@
 <?php
 $field_name = $_POST['cf_name'];
 $field_email = $_POST['cf_email'];
+$field_subject = $_POST['cf_subject'];
 $field_message = $_POST['cf_message'];
 
-$mail_to = 'dedalusonline@yahoo.com';
+$mail_to = 'kvong3@gatech.edu';
 $subject = 'Message from a site visitor '.$field_name;
 
 $body_message = 'From: '.$field_name."\n";
 $body_message .= 'E-mail: '.$field_email."\n";
+$body_message .= 'Suject: '.$field_subject."\n";
 $body_message .= 'Message: '.$field_message;
 
 $headers = 'From: '.$field_email."\r\n";
@@ -17,14 +19,14 @@ $mail_status = mail($mail_to, $subject, $body_message, $headers);
 
 if ($mail_status) { ?>
 	<script language="javascript" type="text/javascript">
-		alert('Thank you for the message. We will contact you shortly.');
+		alert('Message has been sent.');
 		window.location = 'contact_page.html';
 	</script>
 <?php
 }
 else { ?>
 	<script language="javascript" type="text/javascript">
-		alert('Message failed. Please, send an email to gordon@template-help.com');
+		alert('Message failed. Please, send an email to kvong3@gatech.edu');
 		window.location = 'contact_page.html';
 	</script>
 <?php
